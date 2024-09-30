e083b92fbe1e5ef8b33f7ce8864db0ab8ea70c20
dvDaniel <113447778+devdaniel28@users.noreply.github.com>
2024-09-30 16:15:18 -0300

    Terminando o estilo do site e começando com a interação

---
+++ src/script.js
@@ -0,0 +1,11 @@
+function chekerAge() {
+    let dayOfBirth = document.getElementById('dayofbirth')
+    let monthOfBirth = document.getElementById('monthofbirth')
+    let yearOfBirth = document.getElementById('yearofbirth')
+
+    let Age = 0
+
+    if (yearOfBirth.value == 0 || monthOfBirth.value == 0 || dayOfBirth.value == 0) {
+        console.log('inserir dados pls')
+    }
+}
